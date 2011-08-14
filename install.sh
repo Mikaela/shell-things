9f2bf6c295445ac09de83088096dfb1b27ee72a1
Mika Suomalainen <mika.henrik.mainio@hotmail.com>
2011-08-14 18:28:30 +0300

    Added warning to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# This script "installs" files in this gist.
-# WARNING: This will overwrite your .bashrc, .bash_aliases and .bash_profile !!!!!!
-# NOTICE: For this script to work, you must cd to folder where this script is.
+# This script "installs" files in this gist. This script is created by Mika Suomalainen (Mkaysi).
+# WARNING: This will overwrite your .bashrc, .bash_aliases and .bash_profile !
+# NOTICE: For this script to work, you must cd to folder where this script is and run ./install.sh
+#
+#   WARNING: USE THIS SCRIPT WITH YOUR OWN RESPONSIBLITY. AUTHOR OF THIS SCRIPT IS NOT RESPONSIBLE
+#   OF ANY HARMS WHICH MIGHT BE CAUSED BY THIS SCRIPT. NOTICE THAT AS SAID BEFORE, THIS WILL
+#   OVERWRITE YOUR CURRENT .bashrc, .bash_aliases AND .bash_profile ! YOU HAVE BEEN WARNED!
+#
 echo "Installing bash_aliases."
 cat bash_aliases > ~/.bash_aliases
 echo "bash_aliases installed."
