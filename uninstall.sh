431986634f885a67b6758f0673b33e57afbcc922
Mika Suomalainen <mika.henrik.mainio@hotmail.com>
2011-08-16 13:07:22 +0300

    Added notice about completing of uninstalling.

--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,4 @@ cat ~/.bash_aliases.default.backup > ~/.bash_aliases
 cat ~/.bash_profile.default.backup > ~/.bash_profile
 cat ~/.bash_exports.default.backup > ~/.bash_exports
 cat ~/.bash_functions.default.backup > ~/.bash_functions
+echo "Done! Now you should restart bash or source files again for default files to affect."
