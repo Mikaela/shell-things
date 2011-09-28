b162434638d1dab2f1cc7db27c925581fced3cbc
Mika Suomalainen <mika.henrik.mainio@hotmail.com>
2011-09-28 22:00:11 +0300

    install.sh: removed bash_private_aliases

--- install.sh
+++ install.sh
@@ -12,8 +12,6 @@ touch ~/.bash_exports # Creates .bash_exports (this is my idea, I am surprised i
 cat ~/.bash_exports > ~/.bash_exports.default.backup # Backs up .bash_exports.
 touch ~/.bash_functions # I am surprised if you have this, see above.
 cat ~/.bash_functions > ~/.bash_functions.default.backup # Backs up .bash_functions .
-touch ~/.bash_private_aliases # Creates .bash_private_aliases if you don't have one already.
-cat ~/.bash_private_aliases > ~/.bash_private_aliases.default.backup
 echo "Done!"
 echo "Installing bash_aliases."
 cat bash_aliases > ~/.bash_aliases
@@ -30,14 +28,4 @@ echo "bash_exports installed."
 echo "Installing bash_functions"
 cat bash_functions > ~/.bash_functions
 echo "bash_functions installed."
-echo "Installing bash_private_aliases"
-cat bash_private_aliases > ~/.bash_private_aliases
-echo "bash_private_aliases installed"
-#echo "Sourcing bash_aliases..."
-#source ~/.bash_aliases
-#echo "Sourcing bashrc..."
-#source ~/.bashrc
-# echo "Sourcing bash_profile..."
-# source bash_profile
-## The above is not required, because my bash_profile only sources bashrc.
 echo "Installation has completed. Now run 'source ~/.bashrc' to enable the new things."
