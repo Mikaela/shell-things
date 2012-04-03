9853365a9bea88016de8b89b15ed97836e46bda5
Mika Suomalainen <mika.henrik.mainio@hotmail.com>
2012-04-03 13:18:29 +0300

    xsessionrc: Add $HOME/.local to $PATH.

--- .cat.sh
+++ .cat.sh
@@ -7,6 +7,7 @@ cat zshrc > ~/.zshrc
 cat vimrc > ~/.vimrc
 #gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys A4271AC5 # Run gpg once to get configuration file and keyrings.
 #cat gpg.conf > ~/.gnupg/gpg.conf # Maybe this shouldn't get replaced so much, since it contains my key ids.
+cat xsessionrc > ~/.xsessionrc
 exec $SHELL
 
 # vim : set ft=sh :
