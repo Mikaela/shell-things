9f12d233b124f0b4431bbe279543bc521c10ea86
Mika Suomalainen <mika.henrik.mainio@hotmail.com>
2012-04-30 19:28:04 +0300

    .cat.sh: Fix the GPG part.

--- .cat.sh
+++ .cat.sh
@@ -3,10 +3,11 @@
 cat bashrc > ~/.bashrc
 cat tmux.conf > ~/.tmux.conf
 cat zshrc > ~/.zshrc
-cat gitconfig > ~/.gitconfig # I usually do not want this to be overridden so much.
+cat gitconfig > ~/.gitconfig
 cat vimrc > ~/.vimrc
-#gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys A4271AC5 # Run gpg once to get configuration file and keyrings.
-cat gpg.conf > ~/.gnupg/gpg.conf # Maybe this shouldn't get replaced so much, since it contains my key ids.
+mkdir -p ~/.gnupg
+cat gnupg/gpg.conf > ~/.gpg.conf
+cat gpg.conf > ~/.gnupg/gpg.conf
 cat xsessionrc > ~/.xsessionrc
 exec $SHELL
 
