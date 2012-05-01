cf859ce2d560881896d492add06dc8c278ae6b04
Mika Suomalainen <mika.henrik.mainio@hotmail.com>
2012-05-01 15:09:21 +0300

    .cat.sh: add .oidentd.conf

--- .cat.sh
+++ .cat.sh
@@ -8,6 +8,10 @@ cat vimrc > ~/.vimrc
 mkdir -p ~/.gnupg
 cat gnupg/gpg.conf > ~/.gnupg/gpg.conf
 cat xsessionrc > ~/.xsessionrc
+cat oidentd.conf > ~/.oidentd.conf
+chmod 644 ~/.oidentd.conf
+chmod 711 ~
+
 exec $SHELL
 
 # vim : set ft=sh :
