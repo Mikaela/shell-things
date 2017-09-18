73d3d5137724657193d8c8bcf765f01b34c2aad1
Mikaela Suomalainen <mikaela+git@mikaela.info>
2017-09-18 22:05:12 +0300

    move compose key to menu (from lwin)
    Closes #90

--- etc/profile.d/mikaela.sh
+++ etc/profile.d/mikaela.sh
@@ -1,6 +1,6 @@
-# Map compose to left Windows key and kill x with ctrl alt backspace and
+# Map compose to left menu and kill x with ctrl alt backspace and
 # disable unbreakable space
-setxkbmap -option compose:lwin -option terminate:ctrl_alt_bksp -option nbsp:none
+setxkbmap -option compose:menu -option terminate:ctrl_alt_bksp -option nbsp:none
 
 # Set TZ as the current timezone. This is supposed to be sent over SSH.
 TZ=$(date +%Z)
