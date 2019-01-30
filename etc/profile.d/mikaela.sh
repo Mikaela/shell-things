f7fbf35109bd80ecac5601f51a5ed85cacf7a35f
Mikaela Suomalainen <mikaela+git@mikaela.info>
2019-01-30 19:16:45 +0200

    update setxkbmap and hope no one else is using these files

--- etc/profile.d/mikaela.sh
+++ etc/profile.d/mikaela.sh
@@ -1,6 +1,7 @@
 # Map compose to left menu and kill x with ctrl alt backspace and
-# disable unbreakable space
-setxkbmap -option compose:menu -option terminate:ctrl_alt_bksp -option nbsp:none
+# disable unbreakable space, male alt+shift change layout and
+# layouts Finnish, Czech Qwerty and Esperanto.
+setxkbmap -option compose:menu -option terminate:ctrl_alt_bksp -option nbsp:none -option 'grp:alt_shift_toggle' -layout fi,cz,epo -variant ,qwerty,
 
 # Set TZ as the current timezone. This is supposed to be sent over SSH.
 TZ=$(date +%Z)
