12b05ab77a2c64544c6b2afe41eb30fd323de3f3
Mikaela Suomalainen <mikaela+digitalents@mikaela.info>
2021-10-01 13:58:32 +0300

    SetSNTPServers.bat: use all pool addresses just in case
    SNTP is not Chrony that I am used to

--- Windows/SetSNTPServers.bat
+++ Windows/SetSNTPServers.bat
@@ -3,7 +3,7 @@ echo off
 echo Set Time Servers?
 pause
 
-w32tm /config /syncfromflags:manual /manualpeerlist:"time.cloudflare.com nts.netnod.se time.mikes.fi pool.ntp.org"
+w32tm /config /syncfromflags:manual /manualpeerlist:"time.cloudflare.com nts.netnod.se time.mikes.fi 0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org"
 
 pause
 echo on
