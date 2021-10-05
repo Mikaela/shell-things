9d6bcd0832637c5243e350d0ba32042658a05f78
Mikaela Suomalainen <mikaela+digitalents@mikaela.info>
2021-10-05 10:28:21 +0300

    Windows: document time/ & rm SetSNTPServers.bat
    Resolves: #103
    Ref: #102

--- Windows/SetSNTPServers.bat
+++
@@ -1,9 +0,0 @@
-echo off
-
-echo Set Time Servers?
-pause
-
-w32tm /config /syncfromflags:manual /manualpeerlist:"time.cloudflare.com nts.netnod.se time.mikes.fi 0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org"
-
-pause
-echo on
