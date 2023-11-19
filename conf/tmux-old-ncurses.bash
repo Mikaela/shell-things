fcec48698d295a77d5202a56dfa4820e2ea75f18
Aminda Suomalainen <suomalainen+git@mikaela.info>
2023-11-19 20:49:33 +0200

    conf: rm tmux-old-ncurses.bash as legacy

--- conf/tmux-old-ncurses.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Intended for systems with ncurses < 6 which is missing TERMINFO
-# for tmux-256color.
-if [[ $TERM == 'tmux-256color' ]]; then
-	export TERM=screen-256color
-fi
