b770e2f51da300cfc290f92cbbd5b363b81035f2
Aminda Suomalainen <suomalainen+git@mikaela.info>
2023-11-30 10:38:06 +0200

    etc/wireplumber: don't artificially restrict codecs

---
+++ etc/wireplumber/bluetooth.lua.d/61-more-codecs.lua
@@ -0,0 +1,9 @@
+-- https://steamdecki.org/Steam_Deck/Wireless/Bluetooth#Enabling_More_Codecs_and_Enabling_Headsets
+-- Note to self: don't start limiting the codecs, just select the correct one
+-- from pavucontrol, it will be remembered. SteamOS: add pavucontrol as a
+-- non-Steam game for management in big picture mode.
+bluez_monitor.properties = {
+	["bluez5.enable-sbc-xq"] = true,
+	["bluez5.enable-msbc"] = true,
+	["bluez5.codecs"] = "[sbc sbc_xq aac ldac aptx aptx_hd aptx_ll aptx_ll_duplex faststream faststream_duplex]",
+}
