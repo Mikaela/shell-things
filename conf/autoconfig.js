e28f7ff93ea2a85a5b5576e8a6ca7fb2f4a8636b
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-02-14 11:17:59 +0200

    firefox autoconfig
    Resolves: Mikaela/mikaela.github.io#227

---
+++ conf/autoconfig.js
@@ -0,0 +1,8 @@
+// This file belongs to Firefox `default/pref` directory.
+// E.g. /usr/lib64/firefox/defaults/pref/ or ~/.local/firefox/defaults/pref/
+
+// However the file below belongs to ../../ e.g. /usr/lib/64/firefox/ or
+// ~/.local/firefox/
+pref("general.config.filename", "librewolf.overrides.cfg");
+pref("general.config.obscure_value", 0);
+// vim: filetype=javascript
