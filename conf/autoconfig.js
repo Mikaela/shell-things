c349dd04ef6c6f03af9ba49a9f0a1cd062034d2a
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-05-19 20:42:49 +0300

    Revert and WARN about autoconfig logPref
    Apparently it's not allowed and as a result all settings specified there
    vanish. Oops.
    
    This reverts commit ae08cd834ecea27b1dd2976e1a975612546a7405.

--- conf/autoconfig.js
+++ conf/autoconfig.js
@@ -1,9 +1,11 @@
 // This file belongs to Firefox `default/pref` directory.
 // E.g. /usr/lib64/firefox/defaults/pref/ or ~/.local/firefox/defaults/pref/
 
+// WARNING: lockPref() IS NOT ALLOWED HERE!
+
 // However the file below belongs to ../../ e.g. /usr/lib/64/firefox/ or
 // ~/.local/firefox/
-//lockPref("general.config.filename", "librewolf.overrides.cfg");
-lockPref("general.config.filename", "firefox-forbidden-policies.js");
-lockPref("general.config.obscure_value", 0);
+//pref("general.config.filename", "librewolf.overrides.cfg");
+pref("general.config.filename", "firefox-forbidden-policies.js");
+pref("general.config.obscure_value", 0);
 // vim: filetype=javascript
