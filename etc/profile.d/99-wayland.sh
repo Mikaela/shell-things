a8bfd98c0fd1626feaf3c3805fcb0cd782f50547
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-07-17 09:09:12 +0300

    etc/profile.d: add 99-wayland.sh

---
+++ etc/profile.d/99-wayland.sh
@@ -0,0 +1,7 @@
+#export QT_QPA_PLATFORMTHEME="kde"
+export GDK_BACKEND=wayland
+export QT_QPA_PLATFORM=wayland
+export CLUTTER_BACKEND=wayland
+export SDL_VIDEODRIVER=wayland
+export ELECTRON_OZONE_PLATFORM_HINT=wayland
+export MOZ_ENABLE_WAYLAND=1
