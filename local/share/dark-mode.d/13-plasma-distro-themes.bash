83f7b5b447e96e9746358217629591b4660765bb
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-07-20 08:13:52 +0300

    darkman: script for detecting distro specific themes

---
+++ local/share/dark-mode.d/13-plasma-distro-themes.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+. ~/.local/share/dark-mode.d/00-do-not-repeat-yourself.sh
+
+if [[ "$DISTROID" == "Kali" ]]; then
+	lookandfeeltool --apply "Kali-Purple-Dark"
+fi
--- local/share/light-mode.d/zz-kde-plasma-theme-kalipurple.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-lookandfeeltool --apply "Kali-Purple-Light"
