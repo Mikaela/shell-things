23ee3d5e02de24cb58af20d8c2cb221974630e1a
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-07-22 14:55:52 +0300

    darkman/00-do-not-repeat-yourself.bash: quote things in hopes of fixing things

--- local/share/dark-mode.d/00-do-not-repeat-yourself.bash
+++ local/share/dark-mode.d/00-do-not-repeat-yourself.bash
@@ -1,39 +1,41 @@
 #!/usr/bin/env bash
 
+set -x
+
 if ! hash lsb_release 2> /dev/null; then
 	ERRORMESSAGE="lsb_release was not found, the wallpaper scripts cannot figure out the distribution"
-	echo $ERRORMESSAGE
-	notify-send --app-name=darkman --icon=error $ERRORMESSAGE
+	echo "$ERRORMESSAGE"
+	notify-send --app-name=darkman --icon=error "$ERRORMESSAGE"
 	exit 0
 fi
 
 if ! hash kvantummanager 2> /dev/null; then
 	ERRORMESSAGE="kvantummanager wasn't found, Qt theme switching won't work!"
-	echo $ERRORMESSAGE
-	notify-send --app-name="darkman" --icon=error $ERRORMESSAGE
+	echo "$ERRORMESSAGE"
+	notify-send --app-name="darkman" --icon=error "$ERRORMESSAGE"
 	exit 0
 fi
 
-export DISTROID=$(lsb_release -si)
-export DISTROREL=$(lsb_release -sr)
+export DISTROID="$(lsb_release -si)"
+export DISTROREL="$(lsb_release -sr)"
 
-export DARKMANGET=$(darkman get)
+export DARKMANGET="$(darkman get)"
 
 printf "Distribution: $DISTROID\nRelease: $DISTROREL\nMode: $DARKMANGET\n"
 
-if [[ "$DARKMANGET" == "dark" ]]; then
+if [[ "$DARKMANGET" -eq "dark" ]]; then
 	(notify-send --app-name="darkman" --urgency=low --icon=weather-clear-night "Teema: $DARKMANGET" &)
-elif [[ "$DARKMANGET" == "light" ]]; then
+elif [[ "$DARKMANGET" -eq "light" ]]; then
 	(notify-send --app-name="darkman" --urgency=low --icon=weather-clear "Teema: $DARKMANGET" &)
 else
 	ERRORMESSAGE="Something went wrong?"
-	echo $ERRORMESSAGE
-	notify-send --app-name="darkman" --icon=error $ERRORMESSAGE
+	echo "$ERRORMESSAGE"
+	notify-send --app-name="darkman" --icon=error "$ERRORMESSAGE"
 fi
 
 if [[ "$QT_STYLE_OVERRIDE" != "kvantum" ]]; then
 	ERRORMESSAGE="\$QT_STYLE_OVERRIDE is not 'kvantum', Qt theme switching has no effect!"
 	echo $ERRORMESSAGE
-	notify-send --app-name="darkman" --icon=error $ERRORMESSAGE
+	notify-send --app-name="darkman" --icon=error "$ERRORMESSAGE"
 	exit 0
 fi
