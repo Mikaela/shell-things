286694a0c0fa2981335266ea94409f77e5c0910a
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-07-28 07:31:53 +0300

    {rc/{bash,zsh}rc,profile.d/99-brew.sh}: remove duplicated bath

--- etc/profile.d/99-brew.sh
+++ etc/profile.d/99-brew.sh
@@ -1,5 +1,4 @@
 # https://github.com/Homebrew/brew
 if [ -d /home/linuxbrew/.linuxbrew/bin ]; then
-	PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 	eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 fi
