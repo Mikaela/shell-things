c4b20f481d3918654b5f98a4d8d203b768375207
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-07-28 09:50:57 +0300

    {bash,zsh}rc & profile.d: fix if nvm condition

--- etc/profile.d/99-brew.sh
+++ etc/profile.d/99-brew.sh
@@ -2,3 +2,9 @@
 if [ -d /home/linuxbrew/.linuxbrew/bin ]; then
 	eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 fi
+
+if [ -d /home/linuxbrew/.linuxbrew/opt/nvm ]; then
+	export NVM_DIR=/home/linuxbrew/.nvm
+	[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"                                       # This loads nvm
+	[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+fi
