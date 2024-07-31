1ead0a4f9d80db1b0c1be579168abd159b04a9d9
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-07-31 10:48:13 +0300

    darkman/dark: attempt to use breeze dark anyway

--- local/share/dark-mode.d/10-adwaita-breeze-dark.bash
+++ local/share/dark-mode.d/10-adwaita-breeze-dark.bash
@@ -6,11 +6,13 @@
 
 # GTK Theme
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface gtk-theme Adwaita
-dbus-send --session --dest=org.kde.GtkConfig --type=method_call /GtkConfig org.kde.GtkConfig.setGtkTheme "string:Adwaita"
-
+#gsettings set org.gnome.desktop.interface gtk-theme Adwaita
+#dbus-send --session --dest=org.kde.GtkConfig --type=method_call /GtkConfig org.kde.GtkConfig.setGtkTheme "string:Adwaita"
+gsettings set org.gnome.desktop.interface gtk-theme Breeze-Dark
+dbus-send --session --dest=org.kde.GtkConfig --type=method_call /GtkConfig org.kde.GtkConfig.setGtkTheme "string:Breeze-Dark"
 # Xfce theme?e
-xfconf-query --channel xsettings --create --type string --property /Net/ThemeName --set Adwaita
+#xfconf-query --channel xsettings --create --type string --property /Net/ThemeName --set Adwaita
+xfconf-query --channel xsettings --create --type string --property /Net/ThemeName --set Breeze-Dark
 
 # Plasma
 lookandfeeltool --platform offscreen --apply "org.kde.breezedark.desktop"
