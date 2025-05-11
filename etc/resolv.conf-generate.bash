8f978bfdd0191a6375971f365a95df8afd7eef86
Aminda Suomalainen <suomalainen@aminda.eu>
2025-05-11 09:31:43 +0300

    etc/*resolv.conf*.bash: more Fedora Atomic magick

--- etc/resolv.conf-generate.bash
+++ etc/resolv.conf-generate.bash
@@ -19,10 +19,19 @@ rm -v /etc/resolv.conf
 # tee -p = operate in a more appropriate MODE with pipes.
 printf 'nameserver ::1\nnameserver 127.0.0.1\nnameserver 127.0.0.53\nsearch .\noptions timeout:1 attempts:5 rotate edns0 trust-ad\n' | tee -p /etc/resolv.conf
 
-# Remove all other permissions than everyone reading resolv.conf
-chmod -v a=r /etc/resolv.conf
-# Make resolv.conf immutable again so it's pretty sure nothing else edits it.
-#chattr -V +i /etc/resolv.conf
+# Are we on Fedora Atomic?
+if [[ ! -d /sysroot/ostree ]]; then
+	# Remove all other permissions than everyone reading resolv.conf
+	chmod -v a=r /etc/resolv.conf
+	# Make resolv.conf immutable again so it's pretty sure nothing else edits it
+	chattr -V +i /etc/resolv.conf
+else
+	# We are on Fedora Atomic and thus won't want to cause additional errors
+	chmod -v 0644 /sysroot/ostree/deploy/fedora/deploy/*/etc/resolv.conf | true
+	chmod -v 0644 /sysroot/ostree/repo/extensions/rpmostree/private/commit/usr/etc/resolv.conf | true
+	chattr -V -i /sysroot/ostree/deploy/fedora/deploy/*/etc/resolv.conf | true
+	chattr -V -i /sysroot/ostree/repo/extensions/rpmostree/private/commit/usr/etc/resolv.conf | true
+fi
 
 # Let's just see it's ok
 ls -l /etc/resolv.conf
