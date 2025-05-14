0a9896314f21c1bebcaef26b7234953c6dc70488
Aminda Suomalainen <suomalainen@aminda.eu>
2025-05-14 14:10:44 +0300

    fix-networkmanager-iwd-selinux.bash: accidentally become oneliner find
    The original script didn't handle spaces in SSIDs and then I noticed warning about find -exec being better than find in for which may be unreliable and after some manual reading I ended up with this.

--- var/lib/iwd/fix-networkmanager-iwd-selinux.bash
+++ var/lib/iwd/fix-networkmanager-iwd-selinux.bash
@@ -4,7 +4,5 @@
 # permitted to unlink files.
 
 set -x
-for targetfile in $(find /var/lib/iwd/*.{psk,open}); do
-	semanage fcontext -a -t NetworkManager_etc_rw_t $targetfile
-done
+find /var/lib/iwd/*.{psk,open} -exec semanage fcontext -a -t NetworkManager_etc_rw_t '{}' \;
 set +x
