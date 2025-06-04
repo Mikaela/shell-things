10e660eb25ee37a9a3820b1ba8ee0c73fcd59a2f
Aminda Suomalainen <suomalainen@aminda.eu>
2025-06-04 09:54:45 +0300

    profile.d/01-mangohud.sh: add configuration

--- etc/profile.d/01-mangohud.sh
+++ etc/profile.d/01-mangohud.sh
@@ -1,2 +1,3 @@
 # Enables mangohud for everything natively supporting it (outside of flatpak?)
 export MANGOHUD=1
+export MANGOHUD_CONFIG=read_cfg,background_alpha=0,battery_time,battery,cpu_temp,display_server,gpu_temp,device_battery=gamepad+mouse,fan,gamemode,media_player,hud_compact,ram,vram,swap,time,time_no_label,time_format=%F\ %R,wine,network
