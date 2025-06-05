00b1b7523c34ccbb6280f78c5898b458dd03d7c0
Aminda Suomalainen <suomalainen@aminda.eu>
2025-06-05 08:36:04 +0300

    TODO: mangohud should be in a config file instead of env

--- etc/profile.d/01-mangohud.sh
+++ etc/profile.d/01-mangohud.sh
@@ -1,3 +1,3 @@
 # Enables mangohud for everything natively supporting it (outside of flatpak?)
 export MANGOHUD=1
-export MANGOHUD_CONFIG=read_cfg,background_alpha=0,battery_time,battery,cpu_temp,display_server,gpu_temp,device_battery=gamepad+mouse,fan,gamemode,media_player,hud_compact,ram,vram,swap,time,time_no_label,time_format=%F\ %R,wine,network
+export MANGOHUD_CONFIG="read_cfg,background_alpha=0,battery_time,battery,cpu_temp,display_server,gpu_temp,device_battery_icon,device_battery=gamepad+mouse,fan,frame_timing=0,gamemode,media_player,horizontal,hud_compact,ram,vram,swap,show_fps_limit,resolution,time,time_no_label,time_format=%F\ %R,wine,network=eth0,wlan0,mangoapp_steam"
