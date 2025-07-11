fdeeaa39632c09cbdafa93e49e2c57078503164e
Aminda Suomalainen <suomalainen@aminda.eu>
2025-07-11 10:11:25 +0300

    systemd: bring some socket awareness especially to aminda-nocron-reboot.service

--- etc/systemd/system/tlp-masker.bash
+++ etc/systemd/system/tlp-masker.bash
@@ -10,11 +10,11 @@ systemctl enable tlp.service
 
 # Stop the services if they are running
 systemctl disable --quiet --now power-profiles-daemon.service
-systemctl disable --quiet --now systemd-rfkill.service
+systemctl disable --quiet --now systemd-rfkill.{service,socket}
 
 # Actual masking
 systemctl mask power-profiles-daemon.service
-systemctl mask systemd-rfkill.service systemd-rfkill.socket
+systemctl mask systemd-rfkill.service systemd-rfkill.{service,socket}
 
 # In case this script is ran multiple times in succession like when I did
 # when writing it
