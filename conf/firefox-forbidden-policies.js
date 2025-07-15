e4dca1f4690d1ac4188bec55f82f5544d0856ba8
Aminda Suomalainen <suomalainen@aminda.eu>
2025-07-15 20:35:27 +0300

    firefox: attempt to bring back OpenDyslexic again

--- conf/firefox-forbidden-policies.js
+++ conf/firefox-forbidden-policies.js
@@ -32,19 +32,19 @@ lockPref(
 // once it returns to Firefox! https://github.com/OpenDyslexic/extension/issues/75
 lockPref(
 	"font.name-list.sans-serif.x-cyrillic",
-	"Inclusive Sans, Roboto, Liberation Sans, Noto Sans, sans-serif",
+	"OpenDyslexic, Inclusive Sans, Roboto, Liberation Sans, Noto Sans, sans-serif",
 );
 lockPref(
 	"font.name-list.sans-serif.x-western",
-	"Inclusive Sans, Liberation Sans, Comic Neue, Roboto, Noto Sans, sans-serif",
+	"OpenDyslexic, Inclusive Sans, Liberation Sans, Comic Neue, Roboto, Noto Sans, sans-serif",
 );
 lockPref(
 	"font.name-list.serif.x-cyrillic",
-	"Inclusive Sans, Liberation Serif, Roboto Serif, Noto Serif, serif",
+	"OpenDyslexic, Inclusive Sans, Liberation Serif, Roboto Serif, Noto Serif, serif",
 );
 lockPref(
 	"font.name-list.serif.x-western",
-	"Inclusive Sans, Liberation Serif, Roboto Serif, Noto Serif, serif",
+	"OpenDyslexic, Inclusive Sans, Liberation Serif, Roboto Serif, Noto Serif, serif",
 );
 // clearPref("font.name-list.monospace.x-cyrillic");
 // clearPref("font.name-list.monospace.x-western");
