61364717d839a3a016e30c64f7d7cdcc170aecc9
Aminda Suomalainen <suomalainen@aminda.eu>
2025-12-16 12:39:13 +0200

    firefox: clear language settings

--- conf/firefox-forbidden-policies.js
+++ conf/firefox-forbidden-policies.js
@@ -58,7 +58,8 @@ lockPref(
 lockPref("image.animation_mode", "once");
 
 // Spoof en-US as language to scripts
-lockPref("privacy.spoof_english", 2);
+//lockPref("privacy.spoof_english", 2);
+clearPref("privacy.spoof_english");
 
 // More tunable privacy.resistfingerprinting.
 // Refer to https://searchfox.org/mozilla-central/source/toolkit/components/resistfingerprinting/RFPTargets.inc
