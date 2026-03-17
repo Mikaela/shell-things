6b2f6cbd9ce838d1de5d5b2f781ee0df25a5973f
Aminda Suomalainen <suomalainen@aminda.eu>
2026-03-17 09:42:51 +0200

    conf/MangoHud.conf: add winesync

--- etc/profile.d/01-mangohud.sh
+++ etc/profile.d/01-mangohud.sh
@@ -1,3 +1,3 @@
 # Enables mangohud for everything natively supporting it (outside of flatpak?)
 export MANGOHUD=1
-export MANGOHUD_CONFIG='read_cfg,background_alpha=0,horizontal,time,time_no_label,time_format=%X,position=bottom-left'
+export MANGOHUD_CONFIG='read_cfg,background_alpha=0,horizontal,time,time_no_label,time_format=%X,position=bottom-left,winesync'
